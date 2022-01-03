6d8937eaa4699f30ad5060cb1f344f744b453c52
Alexandre Cavalcanti <alexfullops@gmail.com>
2022-01-03 17:19:22 -0300

    run commits

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,11 +1,54 @@
 function calculator (){
-    const operation = prompt('Choose operation: \n 1 - Sum (+)\n 2 - Subtraction (-)\n 3 - Multiplication (*)\n 4 - Diviion (/)\n 5 - Entire division (%)\n 6 - Potentiation (**) ');
+    const operation = Number(prompt('Choose operation: \n 1 - Sum (+)\n 2 - Subtraction (-)\n 3 - Multiplication (*)\n 4 - Diviion (/)\n 5 - Entire division (%)\n 6 - Potentiation (**) '));
     let n1 = Number(prompt('Enter the first value:'));
     let n2 = Number(prompt('Enter the second value:'));
     let result;
     function sum(){
         result = n1 + n2;
         alert(`${n1} + ${n2} = ${result}`);
+        newOperation();
+        } 
+    }
+    function Subtraction(){
+        result = n1 - n2;
+        alert(`${n1} - ${n2} = ${result}`);
+        newOperation();
+        } 
+   
+    function Multiplication(){
+        result = n1 * n2;
+        alert(`${n1} * ${n2} = ${result}`);
+        newOperation();
+        } 
+    
+    function Division(){
+        result = n1 / n2;
+        alert(`${n1} / ${n2} = ${result}`);
+        newOperation();
+        } 
+    
+    function entireDivision(){
+        result = n1 % n2;
+        alert(`The rest of the division between ${n1} % ${n2} = ${result}`);
+        newOperation();
+        } 
+    
+    function potentiation(){
+        result = n1 ** n2;
+        alert(`${n1} ** ${n2} = ${result}`);
+        newOperation();
+        } 
+    
+    function newOperation(){
+        let option = prompt('Do you want to do another operation?\n 1 - Yes\n 2 - No');
+        if (option == 1){
+            calculator();
+        }else if (option == 2){
+            alert('BYE!');
+        }else{
+            alert('Please enter a valid option.')
+            newOperation();
+        } 
     }
     if (operation  == 1){
         sum();
@@ -16,9 +59,9 @@ function calculator (){
     }else if (operation  == 4){
         Division();
     }else if (operation  == 5){
-        EntireDivision();
+        entireDivision();
     }else if (operation  == 6){
-        Pontentiation();
+        potentiation();
     }
-}
+
 calculator();

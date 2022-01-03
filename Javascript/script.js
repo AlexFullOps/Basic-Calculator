59ec9b4c87b6f10e8a2f7f92b527b3c7127dba63
Alexandre Cavalcanti <alexfullops@gmail.com>
2022-01-03 17:59:41 -0300

    rules surf

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,67 +1,73 @@
 function calculator (){
     const operation = Number(prompt('Choose operation: \n 1 - Sum (+)\n 2 - Subtraction (-)\n 3 - Multiplication (*)\n 4 - Diviion (/)\n 5 - Entire division (%)\n 6 - Potentiation (**) '));
-    let n1 = Number(prompt('Enter the first value:'));
+    if (!operation || operation >= 7){
+        alert('Error')
+        calculator();
+    }else{
+        let n1 = Number(prompt('Enter the first value:'));
     let n2 = Number(prompt('Enter the second value:'));
     let result;
+
     function sum(){
         result = n1 + n2;
         alert(`${n1} + ${n2} = ${result}`);
-        newOperation();
-        } 
+        newOperation()
+     } 
     }
     function Subtraction(){
         result = n1 - n2;
         alert(`${n1} - ${n2} = ${result}`);
-        newOperation();
-        } 
+        newOperation()
+    } 
    
     function Multiplication(){
         result = n1 * n2;
         alert(`${n1} * ${n2} = ${result}`);
-        newOperation();
-        } 
+        newOperation()
+    } 
     
     function Division(){
         result = n1 / n2;
         alert(`${n1} / ${n2} = ${result}`);
-        newOperation();
-        } 
+        newOperation()
+    } 
     
     function entireDivision(){
         result = n1 % n2;
         alert(`The rest of the division between ${n1} % ${n2} = ${result}`);
-        newOperation();
-        } 
+        newOperation()
+    } 
     
     function potentiation(){
         result = n1 ** n2;
-        alert(`${n1} ** ${n2} = ${result}`);
-        newOperation();
-        } 
+        alert(`${n1} ** ${n2} = ${result}`)
+        newOperation()
+    } 
     
     function newOperation(){
         let option = prompt('Do you want to do another operation?\n 1 - Yes\n 2 - No');
         if (option == 1){
             calculator();
         }else if (option == 2){
-            alert('BYE!');
+            alert('BYE!')
         }else{
             alert('Please enter a valid option.')
-            newOperation();
+            newOperation()
         } 
     }
-    if (operation  == 1){
+    if (operation == 1){
         sum();
-    }else if (operation  == 2){
+    }else if (operation == 2){
         Subtraction();    
-    }else if (operation  == 3){
+    }else if (operation == 3){
         Multiplication();
-    }else if (operation  == 4){
+    }else if (operation == 4){
         Division();
-    }else if (operation  == 5){
+    }else if (operation == 5){
         entireDivision();
-    }else if (operation  == 6){
+    }else if (operation == 6){
         potentiation();
     }
+}
 
 calculator();
